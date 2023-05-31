91a8c1044b64c61b063dd9887af52546a6ca9ffe
Madhur82954 <itzmadhurverma@gmail.com>
2023-05-31 11:48:32 +0530

    [Madhur] - Queue Added

--- Program.cs
+++ Program.cs
@@ -6,21 +6,20 @@ namespace DataStructure
     {
         static void Main(string[] args)
         {
-           /* QueueEx queue = new QueueEx();
-            queue.Enqueue(20);
-            queue.Enqueue(40);
-            queue.Enqueue(50);
-            queue.Enqueue(60);
+            QueueEx queue = new QueueEx();
+            queue.Enqueue(56);
+            queue.Enqueue(30);
+            queue.Enqueue(70);
             queue.Display();
-            queue.Dequeue();
-            queue.Display();*/
-            StackEx stack = new StackEx();
+            //queue.Dequeue();
+            //queue.Display();
+            /*StackEx stack = new StackEx();
             stack.Push(70);
             stack.Push(30);
             stack.Push(56);
             stack.Display();
             stack.peek();
-            stack.IsEmpty();
+            stack.IsEmpty();*/
            // Console.WriteLine("\n\n");
            // stack.pop();
             //stack.Display(); 
